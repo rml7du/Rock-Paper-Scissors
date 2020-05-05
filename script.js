c07d098c4bad40dc6cbcb663e866ba768c528ca8
Robert Lee <rml7du@gmail.com>
2020-05-05 16:11:05 -0400

    formatting

--- script.js
+++ script.js
@@ -17,17 +17,20 @@ function computerPlay(){
 
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() == computerSelection){
-        console.log( "Tie! Both played " + playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1));
+    if (playerSelection.toLowerCase() == computerSelection){ 
+        console.log( "Tie! Both played " + playerSelection.substring(0,1).toUpperCase() + 
+                playerSelection.substring(1));
         return "tie";
-    } else if (
+    } else if ( 
         (playerSelection.toLowerCase() == 'rock' && computerSelection == "scissors") ||
         (playerSelection.toLowerCase() == 'paper' && computerSelection == "rock") ||
         (playerSelection.toLowerCase() == 'scissors' && computerSelection == "paper")) {
-        console.log( "You Win! " + playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1) + " beats " + computerSelection.substring(0,1).toUpperCase() + computerSelection.substring(1) + ".");
+        console.log( "You Win! " + playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1) + 
+                " beats " + computerSelection.substring(0,1).toUpperCase() + computerSelection.substring(1) + ".");
         return "win";
     } else {
-        console.log( "You Lose! " + computerSelection.substring(0,1).toUpperCase() + computerSelection.substring(1) + " beats " + playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1) + ".");
+        console.log( "You Lose! " + computerSelection.substring(0,1).toUpperCase() + computerSelection.substring(1) + 
+                " beats " + playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1) + ".");
         return "lose";
     }
 }
@@ -39,9 +42,9 @@ function game(){
        const playerSelection = prompt("Rock, Paper, or Scissors", "Rock");
        let outcome = playRound(playerSelection, computerSelection) ;
        if (outcome == "win"){
-           playerCount= playerCount + 1;
+           playerCount = playerCount + 1;
        } else if (outcome == "lose"){
-           computerCount= computerCount + 1;
+           computerCount = computerCount + 1;
        } 
     }
     if(playerCount>computerCount){
