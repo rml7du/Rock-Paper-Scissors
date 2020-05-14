df70ce60f0202c14a111df37496a2725510933ab
Robert Lee <rml7du@gmail.com>
2020-05-14 09:51:57 -0400

    fixed css styles for scoreboard and made progress on counter

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ let playerScore=0;
 let computerScore=0;
 
 const outcome = document.querySelector('#outcome');
-const content = document.createElement('div');
+const content = document.createElement('span');
+
+const player = document.querySelector('#player');
+const playerContent = document.createElement('span');
+
+const computer = document.querySelector('#computer');
+const computerContent = document.createElement('span');
 
 function computerPlay(){
   let rps = Math.floor(Math.random()*3);
@@ -18,45 +24,48 @@ function computerPlay(){
 }
 
 
-function playRound(playerSelection){
+function playRound(playerSelection, playerScore, computerScore){
   const computerSelection = computerPlay();
   
-  
-  
-  if (playerSelection == computerSelection){ 
-        
+  if (playerSelection == computerSelection){   
         content.textContent = "Tie! Both played " + playerSelection.substring(0,1).toUpperCase() + 
                playerSelection.substring(1);
         outcome.appendChild(content);
-      return "tie";
+      return;
   } else if ( 
       (playerSelection == 'rock' && computerSelection == "scissors") ||
       (playerSelection == 'paper' && computerSelection == "rock") ||
       (playerSelection == 'scissors' && computerSelection == "paper")) {
-      
+        
+        playerScore++;
         content.textContent = "You Win! " + playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1) + 
                           " beats " + computerSelection.substring(0,1).toUpperCase() + computerSelection.substring(1) + ".";
         outcome.appendChild(content);
-      return "win";
+
+        playerContent.textContent = playerScore;
+        player.appendChild(playerContent);
+      return;
   } else {
       content.textContent = "You Lose! " + computerSelection.substring(0,1).toUpperCase() + computerSelection.substring(1) + 
               " beats " + playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1) + ".";
       outcome.appendChild(content);
-    return "lose";
+    return computerScore++;
   }
 }
 
-if (playerScore == 0 && computerScore == 0){
 
-}
 
 
       const buttons = document.querySelectorAll('button');
       buttons.forEach((button) => {
         button.addEventListener('click', (e) => { 
-          playRound(button.id);
+          playRound(button.id, playerScore, computerScore);
         });
       });
       
+      if (playerScore == 5) {
+        content.textContent = "You Win! " + playerScore + "to " + computerScore;
+        outcome.appendChild(content);
+      }
       
       
